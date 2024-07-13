abc6cd8f4af81634ee8a2f4d5c20d4fd649ed35c
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2024-07-13 11:44:30 +0300

    Add username value object

---
+++ user_service/domain/value_objects/username.py
@@ -0,0 +1,36 @@
+import re
+
+from dataclasses import dataclass
+
+from user_service.domain.exceptions.value_objects.username import (
+    EmptyUsernameException,
+    InvalidUsernameException,
+    TooLongUsernameException,
+)
+
+from user_service.domain.value_objects.base import BaseValueObject
+
+
+MAX_USERNAME_LENGTH = 22
+USERNAME_PATTERN = re.compile(r"^[a-zA-Z0-9_]+$")
+
+
+@dataclass(frozen=True)
+class Username(BaseValueObject):
+    value: str | None
+
+    def _validate(self) -> None:
+        if self.value is None:
+            return
+        if len(self.value) == 0:
+            raise EmptyUsernameException(self.value)
+        if len(self.value) > MAX_USERNAME_LENGTH:
+            raise TooLongUsernameException(self.value)
+        if not USERNAME_PATTERN.match(self.value):
+            raise InvalidUsernameException(self.value)
+
+    def __str__(self) -> str:
+        return str(self.value) if self.value is not None else ""
+
+    def exists(self) -> bool:
+        return self.value is not None
