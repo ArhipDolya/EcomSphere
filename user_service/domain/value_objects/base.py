c2f0a5b6b211fc19dad1e280157dd58cde54e48c
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2024-07-13 11:43:44 +0300

    Move post init and validation to base value object

--- user_service/domain/value_objects/base.py
+++ user_service/domain/value_objects/base.py
@@ -1,7 +1,12 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
 class BaseValueObject(ABC):
-    ...
+    @abstractmethod
+    def _validate(self) -> None:
+        ...
+
+    def __post_init__(self) -> None:
+        self._validate()
