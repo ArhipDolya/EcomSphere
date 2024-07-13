abd0ed8b67e92fc389e40f71a47f598d39c4afa3
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2024-07-13 15:42:15 +0300

    Add validation for address value object

--- user_service/domain/value_objects/address.py
+++ user_service/domain/value_objects/address.py
@@ -1,4 +1,11 @@
+import re
+
 from dataclasses import dataclass
+
+from user_service.domain.exceptions.value_objects.address import (
+    InvalidAddressException,
+    InvalidZipCodeException,
+)
 
 from user_service.domain.value_objects.base import BaseValueObject
 
@@ -9,3 +16,22 @@ class Address(BaseValueObject):
     street: str
     city: str
     zip_code: str
+
+    def _validate(self) -> None:
+        if not all([self.country, self.street, self.city, self.zip_code]):
+            raise InvalidAddressException("All address fields must be provided")
+
+        if len(self.street) < 3:
+            raise InvalidAddressException("Street must be at least 3 characters long")
+
+        if len(self.city) < 2:
+            raise InvalidAddressException("City must be at least 2 characters long")
+
+        self._validate_zip_code()
+
+    def _validate_zip_code(self):
+        if not re.match(r"^\d{5}(-\d{4})?$", self.zip_code):
+            raise InvalidZipCodeException(f"Invalid zip code format: {self.zip_code}")
+
+    def __str__(self) -> str:
+        return f"{self.street}, {self.city}, {self.zip_code}, {self.country}"
