a0d62bd4c7b5c37e6447f0c261da7f9d4eb9e956
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2024-07-13 16:00:47 +0300

    Change name to username in user event

--- user_service/domain/events/user.py
+++ user_service/domain/events/user.py
@@ -6,19 +6,19 @@ from user_service.domain.events.base import BaseEvent
 @dataclass
 class UserCreatedEvent(BaseEvent):
     user_id: str
-    name: str
+    username: str
     email: str
 
 
 @dataclass
 class UserUpdatedEvent(BaseEvent):
     user_id: str
-    name: str
+    username: str
     email: str
 
 
 @dataclass
 class UserDeletedEvent(BaseEvent):
     user_id: str
-    name: str
+    username: str
     email: str
