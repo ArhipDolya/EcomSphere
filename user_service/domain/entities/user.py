c342394eaba13d56d0619ac39107b7d271ef7d65
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2024-07-13 11:40:27 +0300

    Delete pyright

--- user_service/domain/entities/user.py
+++ user_service/domain/entities/user.py
@@ -7,35 +7,35 @@ from user_service.domain.events.user import UserDeletedEvent, UserUpdatedEvent
 from user_service.domain.value_objects.address import Address
 from user_service.domain.value_objects.password import Password
 from user_service.domain.value_objects.email import Email
+from user_service.domain.value_objects.username import Username
 
 
 @dataclass(frozen=True)
 class User(BaseEntity):
-    """
-    Entity representing a user in the system.
-
-    Attributes:
-        name (str): The user's name.
-        email (Email): The user's email address.
-        password (Password): The user's password.
-        addresses (list[Address]): A list of the user's addresses.
-    """
-
-    name: str
+    username: Username
     email: Email
     password: Password
     addresses: list[Address] = field(default_factory=list)
 
-    def change_email(self, new_email: Email) -> "User":
-        """
-        Change the user's email address.
-
-        Args:
-            new_email (Email): The new email address.
-        """
+    def change_username(self, new_username: Username) -> "User":
         updated_user = User(
             id=self.id,
-            name=self.name,
+            username=new_username,
+            email=self.email,
+            password=self.password,
+            addresses=self.addresses,
+            created_at=self.created_at,
+            updated_at=datetime.now(),
+        )
+        updated_user.register_event(
+            UserUpdatedEvent(user_id=self.id, username=str(new_username), email=str(self.email))
+        )
+        return updated_user
+
+    def change_email(self, new_email: Email) -> "User":
+        updated_user = User(
+            id=self.id,
+            username=self.username,
             email=new_email,
             password=self.password,
             addresses=self.addresses,
@@ -43,20 +43,14 @@ class User(BaseEntity):
             updated_at=datetime.now(),
         )
         updated_user.register_event(
-            UserUpdatedEvent(user_id=self.id, name=self.name, email=str(new_email))
+            UserUpdatedEvent(user_id=self.id, username=str(self.username), email=str(new_email))
         )
         return updated_user
 
     def change_password(self, new_password: Password) -> "User":
-        """
-        Change the user's password.
-
-        Args:
-            new_password (Password): The new password.
-        """
         updated_user = User(
             id=self.id,
-            name=self.name,
+            username=self.username,
             email=self.email,
             password=new_password,
             addresses=self.addresses,
@@ -64,11 +58,11 @@ class User(BaseEntity):
             updated_at=datetime.now(),
         )
         updated_user.register_event(
-            UserUpdatedEvent(user_id=self.id, name=self.name, email=str(self.email))
+            UserUpdatedEvent(user_id=self.id, username=str(self.username), email=str(self.email))
         )
         return updated_user
 
     def delete(self) -> None:
         self.register_event(
-            UserDeletedEvent(user_id=self.id, name=self.name, email=str(self.email))
+            UserDeletedEvent(user_id=self.id, username=str(self.username), email=str(self.email))
         )
