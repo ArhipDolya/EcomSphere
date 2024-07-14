683fa50e75a38009d936f8e6407da1e5636e0236
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2024-07-14 11:44:11 +0300

    Set frozen field to True

--- user_service/domain/events/user.py
+++ user_service/domain/events/user.py
@@ -3,21 +3,21 @@ from dataclasses import dataclass
 from user_service.domain.events.base import BaseEvent
 
 
-@dataclass
+@dataclass(frozen=True)
 class UserCreatedEvent(BaseEvent):
     user_id: str
     username: str
     email: str
 
 
-@dataclass
+@dataclass(frozen=True)
 class UserUpdatedEvent(BaseEvent):
     user_id: str
     username: str
     email: str
 
 
-@dataclass
+@dataclass(frozen=True)
 class UserDeletedEvent(BaseEvent):
     user_id: str
     username: str
