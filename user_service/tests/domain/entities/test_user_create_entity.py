daeb5c78e4472eedfbd29642ad5655ccf6604abe
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2024-07-15 09:38:41 +0300

    Add events to user creation test

--- user_service/tests/domain/entities/test_user_create_entity.py
+++ user_service/tests/domain/entities/test_user_create_entity.py
@@ -1,8 +1,11 @@
 from user_service.domain.entities.user import User
 from user_service.domain.events.user import UserCreatedEvent
+from user_service.domain.value_objects.username import Username
+from user_service.domain.value_objects.email import Email
+from user_service.domain.value_objects.password import Password
 
 
-def test_user_creation(user_data):
+def test_user_creation(user_data: dict[str, Username | Email | Password]) -> None:
     user = User.create(**user_data)
 
     assert user.username == user_data["username"]
@@ -11,8 +14,9 @@ def test_user_creation(user_data):
     assert isinstance(user, User)
 
     # Check if the UserCreatedEvent is registered
-    assert len(user._events) == 1
-    event = user._events[0]
+    events: list[UserCreatedEvent] = user.pull_events()
+    assert len(events) == 1
+    event = events[0]
     assert isinstance(event, UserCreatedEvent)
 
     assert event.user_id == str(user.oid)
