c82e5e574cef963876a497db2604f597941830da
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2024-07-20 15:36:41 +0300

    add error tests for change password functionality

--- user_service/tests/domain/entities/test_change_password.py
+++ user_service/tests/domain/entities/test_change_password.py
@@ -1,8 +1,15 @@
+import pytest
+
 from user_service.domain.entities.user import User
 from user_service.domain.events.user import UserUpdatedEvent
 from user_service.domain.value_objects.email import Email
 from user_service.domain.value_objects.password import Password
 from user_service.domain.value_objects.username import Username
+
+from user_service.domain.exceptions.value_objects.password import (
+    EmptyPasswordException,
+    InvalidPasswordException,
+)
 
 
 def test_change_password(
@@ -27,3 +34,34 @@ def test_change_password(
     assert event.user_id == str(user.oid)
     assert event.username == str(user_data["username"])
     assert event.email == str(user_data["email"])
+
+
+def test_change_password_empty_string(
+    user_data: dict[str, Username | Email | Password]
+) -> None:
+    user = User.create(**user_data)
+
+    with pytest.raises(EmptyPasswordException):
+        new_password = Password("")
+        user.change_password(new_password=new_password)
+
+
+def test_change_password_invalid(
+    user_data: dict[str, Username | Email | Password]
+) -> None:
+    user = User.create(**user_data)
+
+    with pytest.raises(InvalidPasswordException):
+        new_password = Password("password")
+        user.change_password(new_password=new_password)
+
+
+def test_change_password_none(
+    user_data: dict[str, Username | Email | Password]
+) -> None:
+    user = User.create(**user_data)
+
+    with pytest.raises(EmptyPasswordException):
+        new_password = Password(None)
+
+        user.change_password(new_password=new_password)
