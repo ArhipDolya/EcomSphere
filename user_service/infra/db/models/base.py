3034b529a56d36babadc2025e0ad71f8a3baed18
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2024-08-03 19:31:05 +0300

    feat: add base model

--- user_service/infra/db/models/base.py
+++ user_service/infra/db/models/base.py
@@ -1,7 +1,21 @@
-from sqlalchemy import MetaData
-from sqlalchemy.orm import declarative_base
+from datetime import datetime
+from pydantic import BaseModel
+
+from sqlalchemy import MetaData, func
+from sqlalchemy.orm import declarative_base, mapped_column
 
 
 metadata = MetaData()
 
 Base = declarative_base(metadata=metadata)
+
+
+class TimedBaseModel(BaseModel):
+    """An abstract base model that adds created_at and updated_at timestamp fields to the model"""
+
+    __abstract__ = True
+
+    created_at: datetime = mapped_column(default=func.now(), nullable=False)
+    updated_at: datetime = mapped_column(
+        default=func.now(), onupdate=func.now(), nullable=False
+    )
